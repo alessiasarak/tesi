7a9e81d63b87bf22385fb748e235b2d511ecd053
sarakalessia <alessia@hexagonswiss.ch>
2024-04-24 14:07:12 +0200

    feature: card repo basic functionality

--- my-hex-card-backend/repository/cardRepository.js
+++ my-hex-card-backend/repository/cardRepository.js
@@ -1,22 +1,45 @@
 const Card = require('../model/card.js'); 
 
 class CardRepository {
-  async updateCard(cardId, newData) {
-    try {
-      const [updated] = await Card.update(newData, {
-        where: { id: cardId }
-      });
-      return updated;
-    } catch (error) {
-      throw new Error(`Unable to update card: ${error}`);
-    }
-  }
   async getCardById(cardId) {
     try {
       const card = await Card.findByPk(cardId);
       return card;
     } catch (error) {
       throw new Error(`Unable to fetch card: ${error}`);
+    }
+  }
+
+  setCard(idCard, newData) {
+    try {
+      const updated = Card.update(newData, {
+        where: { id: idCard },
+      });
+      return updated;
+    } catch (error) {
+      throw new Error(`Unable to update card: ${error}`);
+    }
+  }
+
+  setStyleCard(idCard, styleName) {
+    try {
+      const updated = Card.update({ fk_name_style: styleName }, {
+        where: { id: idCard },
+      });
+      return updated;
+    } catch (error) {
+      throw new Error(`Unable to update card style: ${error}`);
+    }
+  }
+
+  getStyleCard(idCard) {
+    try {
+      const style = Card.findByPk(idCard, {
+        attributes: ['fk_name_style'],
+      });
+      return style;
+    } catch (error) {
+      throw new Error(`Unable to fetch card style: ${error}`);
     }
   }
 };
