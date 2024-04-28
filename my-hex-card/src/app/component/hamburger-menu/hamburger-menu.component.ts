5b3cfdba26b8878e56273f549f2a6d32e8e372d1
Alessia <alessia.sarak@student.supsi.ch>
2024-04-28 21:47:35 +0200

    fix: hamburger menu

--- my-hex-card/src/app/component/hamburger-menu/hamburger-menu.component.ts
+++ my-hex-card/src/app/component/hamburger-menu/hamburger-menu.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hamburger-menu',
   standalone: true,
-  imports: [],
+  imports: [ MatIconModule ],
   templateUrl: './hamburger-menu.component.html',
   styleUrl: './hamburger-menu.component.css'
 })
 export class HamburgerMenuComponent {
 
+  menu(){
+    var x = document.getElementById("myLinks");
+    
+    if (x!.style.display === "block") {
+      x!.style.display = "none";
+    } else {
+      x!.style.display = "block";
+    }
+  }
 }
