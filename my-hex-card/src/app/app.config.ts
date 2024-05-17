f0a225cce812034bb08be405bb4ad5f4b7694631
sarakalessia <alessia@hexagonswiss.ch>
2024-05-17 08:23:11 +0200

    idk

--- my-hex-card/src/app/app.config.ts
+++ my-hex-card/src/app/app.config.ts
@@ -6,7 +6,7 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation()), 
+    provideRouter(routes), 
     provideHttpClient(),
   ]
 };
