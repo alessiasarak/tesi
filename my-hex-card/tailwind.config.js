64f4b4a3ef6d04f1823c8da034e506868e676447
sarakalessia <alessia@hexagonswiss.ch>
2024-05-22 15:36:29 +0200

    fix: basic for the graphics

--- my-hex-card/tailwind.config.js
+++ my-hex-card/tailwind.config.js
@@ -32,6 +32,27 @@ module.exports = {
     },
     backgroundImage: {
       background: ""
+    },
+    fontSize: {
+      'xs': '0.75rem',
+      'sm': '0.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+
+      title: '35px',
+      subtitle: '16px',
+      tiny_text: '10px',
+    },
+    fontWeight: {
+      title: '700',
+      subtitle: '400',
+      normal: '400',
     }
   },
   plugins: [],
