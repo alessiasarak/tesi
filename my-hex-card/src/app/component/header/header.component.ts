81c12c66705ad86290f9287d8904c8494151ea1e
Alessia <alessia@hexagonswiss.ch>
2024-05-22 21:11:08 +0200

    fix: profile setting style

--- my-hex-card/src/app/component/header/header.component.ts
+++ my-hex-card/src/app/component/header/header.component.ts
@@ -12,8 +12,37 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   isVisible = false;
+  menuList : any[] = [];
+
+  userMenu = [
+    { link: "/settings", title: "Home", active: true },
+    { link: "/profile-settings", title: "Profilo", active: false },
+    { link: "/logout", title: "Logout", active: false }
+  ];
+  adminMenu = [];
+  notLoggedMenu = [
+    { link: "/login", title: "Home", active: true },
+    { link: "https://hexcard.ch/store/", title: "Compra la card", active: false },
+    { link: "https://hexcard.ch/contatti/", title: "Aiuto?", active: false }
+  ];
 
   menu(){
     this.isVisible = !this.isVisible;
+  }
+  setSelected(i : number){
+    for(let item of this.menuList){
+      item.active = false;
+    }
+
+    this.menuList[i].active = true;
+  }
+
+  ngOnInit() {
+    let userLogged = localStorage.getItem("user_id");
+    if(userLogged) {
+      let role = localStorage.getItem("role");
+      if(role && role == "USER") this.menuList = this.userMenu;
+      else if(role && role == "ADMIN") this.menuList = this.adminMenu;
+    }
   }
 }
