b8dd3ea31bc1c882ea7c2a821292f280258ca746
sarakalessia <alessia@hexagonswiss.ch>
2024-05-28 13:30:18 +0200

    fix: card preview singola che carica tutta

--- my-hex-card/src/app/component/card-preview/card-preview.component.ts
+++ my-hex-card/src/app/component/card-preview/card-preview.component.ts
@@ -44,7 +44,5 @@ export class CardPreviewComponent {
   ngOnInit(): void {
     this.myMainStyle = "color: " + this.card.text_color + "; background-color: " + this.card.background_color + ";";
     this.myButtonStyle = "background-color: " + this.card.button_color + ";";
-
-    console.log(this.card)
   }
 }
