22b183c57cc060c879d2b94f47300b265f651c8e
Alessia <alessia@hexagonswiss.ch>
2024-06-04 20:01:19 +0200

    fix

--- my-hex-card-backend/server.js
+++ my-hex-card-backend/server.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 const corsOptions = {
   origin: function(origin, callback) {
-    const allowedOrigins = ["http://localhost:4200", "https://127.0.0.1:4200"];
+    const allowedOrigins = ["https://www.myhexcard.com", "https://myhexcard.com"];
     if(!origin || allowedOrigins.includes(origin)){
       callback(null, true);
     } else {
