b066865231a000e06aabeb5ce63b95a59e950c04
sarakalessia <alessia@hexagonswiss.ch>
2024-07-03 10:29:16 +0200

    feature: base for image cropper

---
+++ my-hex-card/src/app/component/image-cropper/image-cropper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-cropper',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-cropper.component.html',
+  styleUrl: './image-cropper.component.css'
+})
+export class ImageCropperComponent {
+
+}
